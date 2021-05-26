14d4516b988c8fe85812046223f1fa9f81cdacce
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2021-05-25 23:26:30 -0400

    ex docs

--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule SimpleHTTP.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 
   defp description do
